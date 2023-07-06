1fd0978e3f8de91af4a98aed873c5d2d003a0059
Adrian Biro <biroadrian@protonmail.com>
2023-07-06 18:29:53 +0200

    small fixes

--- index.md
+++ index.md
@@ -1,7 +1,7 @@
-# Content 
+﻿# Blogs, Notes, and Stuff
 This page is intended to rescue me in my ongoing struggle to share knowledge in a reusable way, so I will try move it here from emails, gists confluence, Slack, and Teams messages that no-one can find after 2 days.
-## Blogs
-
-* [AWK](./blogs/AWK.md)
-* [KQL vs Lucene in Kibana](./blogs/KQLvsLucene.md)
-* [90. Vibes Website With Powershell, Markdown to HTML](blogs/90.VibeWebsiteWithPowershellMarkdown2HTML.md)
+# Content
+### blogs
+* [90. Vibes Website With Powershell, Markdown to HTML](./content/blogs/90.VibeWebsiteWithPowershellMarkdown2HTML.md)
+* [AWK tips](./content/blogs/AWK.md)
+* [KQL vs Apache Licene](./content/blogs/KQLvsLucene.md)
---
+++ scripts/generate-index.ps1
@@ -0,0 +1,38 @@
+#!/usr/bin/pwsh
+<#
+    .SYNOPSIS
+        
+    
+    .DESCRIPTION  
+        Generate index.md  
+    .NOTES
+        Use file up-to '# Content', after that generate the Index
+    .LINK
+        https://adrianbiro.github.io
+#>
+
+[system.io.fileinfo] $IndexFile = Resolve-Path -Relative "index.md"
+[System.Collections.ArrayList]$Content = @()
+
+foreach ($i in (Get-Content -Path $IndexFile)) {
+    $Content.Add($i) | Out-Null
+    if ($i -match "# Content") { break }; 
+} 
+
+[string[]]$blogs = foreach ($i in (Get-ChildItem -Path "content" -Recurse -Filter '*.md')) { 
+    Resolve-Path -Relative $i 
+}
+
+[System.Collections.ArrayList]$dirs = @()
+
+foreach ($i in (Sort-Object -InputObject $blogs -Descending)) {
+    [system.io.fileinfo]$file = $i
+    [string] $dir = $file.Directory.Name
+    if (-not ($dirs -contains $dir)) {
+        $Content.Add(("### {0}" -f $dir)) | Out-Null
+        $dirs.Add($dir) | Out-Null
+    }
+    $Content.Add(("* [{0}]({1})" -f ((Get-Content -TotalCount 1 -Path $i) -replace "# ", ""), $i)) | Out-Null
+} 
+
+Set-Content -Value $Content -Encoding "utf8BOM" -Path $IndexFile 
