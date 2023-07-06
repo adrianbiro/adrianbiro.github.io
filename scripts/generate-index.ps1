713936c41c22cc502f79ad0b5d1c7405c2ee794f
Adrian Biro <biroadrian@protonmail.com>
2023-07-06 18:44:05 +0200

    small fixes

--- scripts/generate-index.ps1
+++ scripts/generate-index.ps1
@@ -14,17 +14,20 @@
 [system.io.fileinfo] $IndexFile = Resolve-Path -Relative "index.md"
 [System.Collections.ArrayList]$Content = @()
 
+Write-Host "Generating Content"
+
 foreach ($i in (Get-Content -Path $IndexFile)) {
     $Content.Add($i) | Out-Null
     if ($i -match "# Content") { break }; 
 } 
+
+Write-Host "Generating Index"
 
 [string[]]$blogs = foreach ($i in (Get-ChildItem -Path "content" -Recurse -Filter '*.md')) { 
     Resolve-Path -Relative $i 
 }
 
 [System.Collections.ArrayList]$dirs = @()
-
 foreach ($i in (Sort-Object -InputObject $blogs -Descending)) {
     [system.io.fileinfo]$file = $i
     [string] $dir = $file.Directory.Name
@@ -35,4 +38,10 @@ foreach ($i in (Sort-Object -InputObject $blogs -Descending)) {
     $Content.Add(("* [{0}]({1})" -f ((Get-Content -TotalCount 1 -Path $i) -replace "# ", ""), $i)) | Out-Null
 } 
 
+Write-Host("Writing content to {0}" -f $IndexFile)
+
 Set-Content -Value $Content -Encoding "utf8BOM" -Path $IndexFile 
+
+Write-Host("Content:") 
+
+$Content
