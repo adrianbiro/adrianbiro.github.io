884254d547a0fd43aee164015b4920436c5203bd
Adrian Biro <biroadrian@protonmail.com>
2023-07-16 14:56:42 +0200

    fix index

--- scripts/generate-index.ps1
+++ scripts/generate-index.ps1
@@ -10,38 +10,47 @@
     .LINK
         https://adrianbiro.github.io
 #>
-
-[system.io.fileinfo] $IndexFile = Resolve-Path -Relative "index.md"
-[System.Collections.ArrayList]$Content = @()
-
-Write-Host "Generating Content"
-
-foreach ($i in (Get-Content -Path $IndexFile)) {
-    $Content.Add($i) | Out-Null
-    if ($i -match "## Content") { break }; 
-} 
-
-Write-Host "Generating Index"
-
-[string[]]$blogs = foreach ($i in (Get-ChildItem -Path "content" -Recurse -Filter '*.md')) { 
-    Resolve-Path -Relative $i 
+function Set-Index {
+    Param([string] $Location)
+    [string[]]$blogs = foreach ($i in (Get-ChildItem -Path $Location -Recurse -Filter '*.md')) { 
+        Resolve-Path -Relative $i 
+    }
+    
+    [System.Collections.ArrayList]$dirs = @()
+    foreach ($i in (Sort-Object -InputObject $blogs -Descending)) {
+        [system.io.fileinfo]$file = $i
+        [string] $dir = $file.Directory.Name
+        if (-not ($dirs -contains $dir)) {
+            $Content.Add(("### {0}" -f $dir)) | Out-Null
+            $dirs.Add($dir) | Out-Null
+        }
+        $Content.Add(("* [{0}]({1})" -f ((Get-Content -TotalCount 1 -Path $i) -replace "# ", ""), $i)) | Out-Null
+    }
 }
 
-[System.Collections.ArrayList]$dirs = @()
-foreach ($i in (Sort-Object -InputObject $blogs -Descending)) {
-    [system.io.fileinfo]$file = $i
-    [string] $dir = $file.Directory.Name
-    if (-not ($dirs -contains $dir)) {
-        $Content.Add(("### {0}" -f $dir)) | Out-Null
-        $dirs.Add($dir) | Out-Null
+
+[System.Collections.ArrayList]$Content = @()
+function main {
+    [system.io.fileinfo] $IndexFile = Resolve-Path -Relative "index.md"
+    Write-Host "Generating Content"
+
+    foreach ($i in (Get-Content -Path $IndexFile)) {
+        $Content.Add($i) | Out-Null
+        if ($i -match "## Content`n") { break }; 
+    } 
+
+    Write-Host "Generating Index"
+
+    foreach ( $i in (Get-ChildItem "./content/" -Recurse -Filter "*.md").Directory | Sort-Object -Unique) {
+        Set-Index -Location $i
     }
-    $Content.Add(("* [{0}]({1})" -f ((Get-Content -TotalCount 1 -Path $i) -replace "# ", ""), $i)) | Out-Null
-} 
 
-Write-Host("Writing content to {0}" -f $IndexFile)
+    Write-Host("Writing content to {0}" -f $IndexFile)
 
-Set-Content -Value $Content -Encoding "utf8BOM" -Path $IndexFile 
+    Set-Content -Value $Content -Encoding "utf8BOM" -Path $IndexFile 
 
-Write-Host("Content:") 
+    Write-Host("Content:") 
 
-$Content
+    $Content
+}
+main
